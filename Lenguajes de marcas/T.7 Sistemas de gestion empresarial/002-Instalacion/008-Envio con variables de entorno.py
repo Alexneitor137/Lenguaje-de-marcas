60e85fb2d3b0be0024972bace79e5d6c2483de71
Ajolote137 <Alexcalderonsanchez@gmail.com>
2026-01-26 10:16:10 +0100

    A

--- Lenguajes de marcas/T.7 Sistemas de gestion empresarial/002-Instalacion/008-Envio con variables de entorno.py
+++ Lenguajes de marcas/T.7 Sistemas de gestion empresarial/002-Instalacion/008-Envio con variables de entorno.py
@@ -1,14 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+import os
 
-SMTP_SERVER = "smtp.example.com"
+SMTP_SERVER = os.environ.get("EMAIL_SERVIDOR")
 SMTP_PORT = 587          # 587 = STARTTLS, 465 = SSL
-SMTP_USER = "user@example.com"
-SMTP_PASS = "password"
+SMTP_USER = os.environ.get("EMAIL_USUARIO")
+SMTP_PASS = os.environ.get("EMAIL_CONTRASENA")
 
 msg = EmailMessage()
 msg["From"] = "alexa.alexiana@gmx.es"
-msg["To"] = "Alexcalderonsanchez@gmail.com.com"
+msg["To"] = "jocarsa2@gmail.com"
 msg["Subject"] = "Esto es un ejercicio de clase"
 msg.set_content("Hola esto es una prueba desde Python.\n")
 
